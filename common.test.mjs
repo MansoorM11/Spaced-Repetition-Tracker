48a17235ff390b4c86e8064ced182e2119eaa592
Mansoor Munawar <mansoor.munawar448@gmail.com>
2026-02-26 22:54:23 +0000

    created test for revision dates and rename file

--- common.test.mjs
+++ common.test.mjs
@@ -1,7 +1,31 @@
 import { getUserIds } from "./common.mjs";
 import assert from "node:assert";
 import test from "node:test";
+import { calculateRevisionDates } from "./script.mjs";
 
 test("User count is correct", () => {
   assert.equal(getUserIds().length, 5);
+});
+
+test("Calculate correct revision dates drom 2026-03-01", () => {
+  const result = calculateRevisionDates("2026-03-01");
+
+  expect(result).toEqual([
+    "2026-03-08",
+    "2026-04-01",
+    "2026-06-01",
+    "2026-09-01",
+    "2027-03-01",
+  ]);
+});
+
+test("Calculates revision date correctly across year boundary", () => {
+  const result = calculateRevisionDates("2026-12-30");
+  expect(result).toEqual([
+    "2027-01-06",
+    "2027-01-30",
+    "2027-03-30",
+    "2027-06-30",
+    "2027-12-30",
+  ]);
 });
